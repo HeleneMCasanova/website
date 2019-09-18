65cf4f81bf165897fc2fab787c036afe6904f7fc
Helene Casanova <helene_casanova@aol.com>
2019-09-18 17:39:02 -0400

    Updates

---
+++ precache-manifest.5a525c863e5c9392ac052dc115eb89c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d193b37712e8b0b5d9d1880ee1a6f592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014cd4ec29038ffd2828",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7e3305aa767ce9597622",
+    "url": "/static/css/main.6e0f624f.chunk.css"
+  },
+  {
+    "revision": "014cd4ec29038ffd2828",
+    "url": "/static/js/2.a5c7873a.chunk.js"
+  },
+  {
+    "revision": "7e3305aa767ce9597622",
+    "url": "/static/js/main.11e3d4e1.chunk.js"
+  },
+  {
+    "revision": "ca5c8fe1c991f76257d4",
+    "url": "/static/js/runtime~main.105f67c5.js"
+  },
+  {
+    "revision": "5c106c7f746f5597697a1c06eb45fafc",
+    "url": "/static/media/blog.5c106c7f.jpeg"
+  },
+  {
+    "revision": "2afc582c93820a3733ebf657546c4eaa",
+    "url": "/static/media/book_cover.2afc582c.jpg"
+  },
+  {
+    "revision": "d218ac8dcc532cf4efd4efae81e49de1",
+    "url": "/static/media/enigma.d218ac8d.jpg"
+  },
+  {
+    "revision": "5328e919ed422805ce2cd210d125cfd2",
+    "url": "/static/media/escape.5328e919.png"
+  },
+  {
+    "revision": "f3bb5ef8ffbc6a72191a82bfe4d36dba",
+    "url": "/static/media/kw.f3bb5ef8.PNG"
+  },
+  {
+    "revision": "51530a2009ab41cbdf6e2d342cd3d3c1",
+    "url": "/static/media/piano_sim.51530a20.png"
+  }
+]);
